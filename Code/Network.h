803fd2775e9800eb69dd4a87a2a3406983423b32
Gaston Morixe <imton@users.noreply.github.com>
2015-10-14 13:09:05 -0200

    Update Network.h

--- Code/Network.h
+++ Code/Network.h
@@ -18,6 +18,21 @@
 //  limitations under the License.
 //
 
+
+#import <Availability.h>
+
+#define _AFNETWORKING_PIN_SSL_CERTIFICATES_
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED
+  #import <SystemConfiguration/SystemConfiguration.h>
+  #import <MobileCoreServices/MobileCoreServices.h>
+  #import <Security/Security.h>
+#else
+  #import <SystemConfiguration/SystemConfiguration.h>
+  #import <CoreServices/CoreServices.h>
+  #import <Security/Security.h>
+#endif
+
 #import "RKRoute.h"
 #import "RKRouteSet.h"
 #import "RKRouter.h"
